02918a33ddf22e21a3c5b77ca20fe440ffe0e5b9
AlexThomUCF <al849783@ucf.edu>
2025-09-24 00:04:29 -0400

    Playermovement

--- CosmicWageWorkers/Assets/Scripts/PlayerMovement.cs
+++ CosmicWageWorkers/Assets/Scripts/PlayerMovement.cs
@@ -5,31 +5,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    PlayerInput playerInput;
+    Rigidbody rb;
+    Vector2 moveInput; 
 
-    InputAction move;
 
-    [SerializeField] float speed = 5f;  
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 5f;
+
+    bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
-        move = playerInput.actions.FindAction("Move");
+  
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 
     void MovePlayer()
     {
-        Vector2 direction = move.ReadValue<Vector2>();
-        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
-        
+        Vector3 playerVelocity = new Vector3(moveInput.x * speed, rb.linearVelocity.y, moveInput.y * speed);
+        rb.linearVelocity = transform.TransformDirection(playerVelocity);
+    }
 
-
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
