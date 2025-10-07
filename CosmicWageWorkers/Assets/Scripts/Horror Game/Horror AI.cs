09fee501ea80e1e3d735f1e06d0794c52077e814
AlexThomUCF <al849783@ucf.edu>
2025-10-07 00:04:55 -0400

    Horror scripts

--- CosmicWageWorkers/Assets/Scripts/Horror Game/Horror AI.cs
+++ CosmicWageWorkers/Assets/Scripts/Horror Game/Horror AI.cs
@@ -6,16 +6,65 @@ public class HorrorAI : MonoBehaviour
     NavMeshAgent monsterAgent;
     public Transform playerTransform;
 
+    private EnemyVision enemyVision;
+
+    private GameObject newObject;
+    private int tempMark = 0;
+    private bool hasDetected = false;
+    //public Transform childTransform;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //Three states
+    //Base roam State
+    // Player range state
+    // Phase 2 state'
+
+    enum AIState
+    {
+        Base,
+        ZoneState,
+        Phase2State
+    };
+
     void Start()
     {
         monsterAgent = GetComponent<NavMeshAgent>();
+        enemyVision = GetComponent<EnemyVision>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        monsterAgent.SetDestination(playerTransform.position);
+        if(enemyVision.allTrue)
+        {
+            monsterAgent.SetDestination(playerTransform.position);
+        }
+        else
+        {
+            monsterAgent.isStopped = true;
+        }
     }
+
+
+    /*IEnumerator DetectedAttack()// used just for exclamation mark spawn
+    {
+        monsterAgent.isStopped = true;
+
+        if (tempMark == 0)
+        {
+
+            newObject = Instantiate(noticeMark, childTransform.position, Quaternion.identity);
+            //guardAudio.Play();
+            newObject.transform.SetParent(childTransform);
+            tempMark++;
+            Destroy(newObject, 1.8f);
+        }
+
+        yield return new WaitForSeconds(1.8f);
+
+        monsterAgent.isStopped = false;
+        tempMark = 0;
+    }*/
 }
