2ceaeb55e02c00937c448c13b0553cb88c115b95
AlexThomUCF <al849783@ucf.edu>
2025-11-06 21:19:38 -0500

    Lap system

---
+++ CosmicWageWorkers/Assets/Scripts/Racing Game/RaceManager.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class RaceManager : MonoBehaviour
+{
+ public static RaceManager Instance;
+
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI currentLapTimeText;
+    [SerializeField] private TextMeshProUGUI bestLapTimeText;
+    [SerializeField] private TextMeshProUGUI overallRaceTimeText;
+    [SerializeField] private TextMeshProUGUI lapText;
+
+    [Header("Race Settings")]
+    [SerializeField] private Checkpoint[] checkpoints;
+    [SerializeField] private bool isCircuit = false; // defines if checkpoints form a loop(continous circuit), 
+    [SerializeField] private int totalLaps = 1;
+
+    private int lastCheckpointIndex = -1;
+
+    private int currentLap = 1;
+
+    private bool raceStarted = false;
+    private bool raceFinished = false;
+
+    [Header("Lap Timer")]
+    private float currentLapTime = 0f;
+    private float bestLapTime = Mathf.Infinity; 
+    private float overallRaceTime = 0f;
+
+    #region Unity Functions
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (raceStarted)
+        {
+            UpdateTimers();
+        }
+        UpdateUI();
+
+    }
+
+    #endregion
+
+    #region Checkpoint Management
+    public void CheckpointReached(int checkpointIndex)
+    {
+        if ((!raceStarted && checkpointIndex != 0) || raceFinished) return; // check if race hasnt started and checkpoint crossed isn't the first one or if race has finished
+
+        if(checkpointIndex == lastCheckpointIndex + 1)//ensures checkpoints are followed in correct sequence
+        {
+            UpdateCheckpoint(checkpointIndex);
+        }
+    }
+
+    private void UpdateCheckpoint(int checkpointIndex)
+    {
+        if(checkpointIndex == 0)//player has reached starting checkpoint
+        {
+            if(!raceStarted)
+            {
+                StartRace();
+            }
+            else if(isCircuit && lastCheckpointIndex == checkpoints.Length - 1)//if last check point 
+            {
+                OnLapFinish();
+            }
+        }
+        else if( !isCircuit && checkpointIndex == checkpoints.Length - 1)
+        {
+            OnLapFinish();
+        }
+
+        lastCheckpointIndex = checkpointIndex;
+    }
+
+    #endregion
+
+    #region Race Management
+
+    private void OnLapFinish()
+    {
+        currentLap++;
+
+        if (currentLapTime < bestLapTime)
+        {
+            bestLapTime = currentLapTime;
+        }
+
+        if (currentLap > totalLaps)
+        {
+            EndRace();
+        }
+        else
+        {
+            currentLapTime = 0f;
+            lastCheckpointIndex = isCircuit ? 0 : -1;
+        }
+
+      
+    }
+    private void StartRace()
+    {
+        raceStarted = true;
+        raceFinished = false;
+    }
+
+    private void EndRace()
+    {
+        raceFinished = true;
+        raceStarted = false;
+    }
+
+    private void UpdateTimers()
+    {
+        currentLapTime += Time.deltaTime;
+        overallRaceTime += Time.deltaTime;
+    }
+
+    private void UpdateUI()
+    {
+        currentLapTimeText.text = FormatTime(currentLapTime);
+        overallRaceTimeText.text = FormatTime(overallRaceTime);
+        lapText.text = "Lap: " + currentLap + "/" + totalLaps;
+        bestLapTimeText.text = FormatTime(bestLapTime);
+    }
+
+    #endregion
+
+    #region Utility Functions
+
+    private string FormatTime(float time)
+    {
+        if (float.IsInfinity(time) || time < 0) return "--:--";
+
+        int minutes = (int)time / 60;
+        float seconds = time % 60;
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    #endregion
+
+}
