39fbb5d4844938802c19f3a89e11ad9ba3be9249
FerPresto <163638368+FerPresto@users.noreply.github.com>
2026-01-26 02:40:35 -0500

    BathroomPrototype

--- CosmicWageWorkers/Assets/Scripts/Bathroom Horror/BathroomsUnhide.cs
+++ CosmicWageWorkers/Assets/Scripts/Bathroom Horror/BathroomsUnhide.cs
@@ -10,7 +10,11 @@ public class BathroomsUnhide : MonoBehaviour
 
     public GameObject firstSection;
 
+    public GameObject firstLight;
+
     public GameObject secondWall;
+
+    public GameObject secondLight;
 
     public GameObject secondPuddle;
 
@@ -18,9 +22,24 @@ public class BathroomsUnhide : MonoBehaviour
 
     public GameObject thirdWall;
 
+    public GameObject thirdLight;
+
     public GameObject thirdPuddle;
 
     public GameObject thirdSection;
+
+    public GameObject fourthLight;
+
+    public GameObject lastLight;
+
+    public Animator doorAnimator;
+
+    public GameObject lastSoup;
+
+    private bool horrorGameStarted = true; 
+    private bool doorOpened = false;
+
+    public float doorTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,26 +49,78 @@ public class BathroomsUnhide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.transform.position.z > 341)
+        if (player.transform.position.z > 341 && horrorGameStarted)
         {
             firstWall.SetActive(false);
             firstPuddle.SetActive(false);
+            firstLight.SetActive(false);
+            secondLight.SetActive(true);
             firstSection.SetActive(true);
         }
 
-        if (player.transform.position.z > 356)
+        if (player.transform.position.z > 356 && horrorGameStarted)
         {
             secondWall.SetActive(false);
             secondPuddle.SetActive(false);
+            secondLight.SetActive(false);
+            thirdLight.SetActive(true);
             secondSection.SetActive(true);
         }
-        if (player.transform.position.z > 387)
+        if (player.transform.position.z > 387 && horrorGameStarted)
         {
             thirdWall.SetActive(false);
             thirdPuddle.SetActive(false);
+            thirdLight.SetActive(false);
+            fourthLight.SetActive(true);
             thirdSection.SetActive(true);
+
+        }
+
+        if (doorOpened)
+        {
+            doorTimer -= Time.deltaTime;
+            if (doorTimer <= 0)
+            {
+                CloseDoor();
+                doorOpened = false;
+                horrorGameStarted = false;
+                lastSoup.SetActive(true);
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 332f);
+                SectionsTurnedOff();
+
+            }
         }
     }
-    
-    
+
+    public void OpenDoor()
+    {
+        doorAnimator.SetTrigger("DoorOpen");
+        doorOpened = true;
+    }   
+
+    public void CloseDoor()
+    {
+        doorAnimator.SetTrigger("DoorClosed");
+    }
+
+    private void SectionsTurnedOff()
+    {
+        firstSection.SetActive(false);
+        secondSection.SetActive(false);
+        thirdSection.SetActive(false);
+        fourthLight.SetActive(false);
+        firstWall.SetActive(true);
+        firstLight.SetActive(true);
+        lastLight.SetActive(true);
+
+    }
+
+    public void BackToMainScene()
+    {
+        if (!horrorGameStarted)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        }
+    }
+
 }
