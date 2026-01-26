7ba8c28c20d853f9f67b52d2959803619f35626b
FerPresto <163638368+FerPresto@users.noreply.github.com>
2026-01-25 20:54:21 -0500

    BathroomProgress

--- CosmicWageWorkers/Assets/Scripts/Bathroom Horror/BathroomsUnhide.cs
+++ CosmicWageWorkers/Assets/Scripts/Bathroom Horror/BathroomsUnhide.cs
@@ -2,6 +2,25 @@ using UnityEngine;
 
 public class BathroomsUnhide : MonoBehaviour
 {
+    public GameObject player;
+
+    public GameObject firstWall;
+
+    public GameObject firstPuddle;
+
+    public GameObject firstSection;
+
+    public GameObject secondWall;
+
+    public GameObject secondPuddle;
+
+    public GameObject secondSection;
+
+    public GameObject thirdWall;
+
+    public GameObject thirdPuddle;
+
+    public GameObject thirdSection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +30,25 @@ public class BathroomsUnhide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.z > 341)
+        {
+            firstWall.SetActive(false);
+            firstPuddle.SetActive(false);
+            firstSection.SetActive(true);
+        }
+
+        if (player.transform.position.z > 356)
+        {
+            secondWall.SetActive(false);
+            secondPuddle.SetActive(false);
+            secondSection.SetActive(true);
+        }
+        if (player.transform.position.z > 387)
+        {
+            thirdWall.SetActive(false);
+            thirdPuddle.SetActive(false);
+            thirdSection.SetActive(true);
+        }
     }
     
     
