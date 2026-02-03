ff40e0c77e24927c40187ee46e0a058e4998e8be
AlexThomUCF <al849783@ucf.edu>
2026-02-02 20:48:45 -0500

    Working on Prop fps manager
    4 hours

--- CosmicWageWorkers/Assets/Scripts/JackFPS/PropMovement.cs
+++ CosmicWageWorkers/Assets/Scripts/JackFPS/PropMovement.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PropMovement : MonoBehaviour
 {
+    public int propPerShelve = 2;
+    List<GameObject> props = new List<GameObject>();
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SearchForChild();
+        RandomPropMovement();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SearchForChild()
+    {
+        foreach (Transform child in transform)
+        {
+            
+            if(child.CompareTag("Prop"))
+            {
+                props.Add(child.gameObject);
+                
+            }
+        }
+    }
+
+    public void RandomPropMovement()
+    {
+        int propListLength = props.Count;
+        for (int i = 0; i < propPerShelve; i++)
+        {
+            int randomInt = Random.Range(0, propListLength);
+            GameObject randomProp = props[randomInt];
+            Debug.Log("Prop: " + randomProp.name);
+            // randomProp.SetActive(false);
+            //Play animation
+            randomProp.transform.SetParent(null);
+            randomProp.tag = "MovingProp";
+        }
+
+
     }
 }
