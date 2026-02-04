c91de68097c5e39641ce2a96a80a7c15f399df11
FerPresto <163638368+FerPresto@users.noreply.github.com>
2026-02-04 08:57:50 -0500

    AntigravityWhiteboxTesting

--- CosmicWageWorkers/Assets/Scripts/Anti Gravity Game/GravityManager.cs
+++ CosmicWageWorkers/Assets/Scripts/Anti Gravity Game/GravityManager.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class GravityManager : MonoBehaviour
 {
-
-    public AntiGravity antiGravityScript;
     public float gravityScale = -1.0f;
+    public GameObject player;
+    private bool gravityOn = false;
+    public float sceneLoader = 3f;
+    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +18,29 @@ public class GravityManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.y < 7.0f)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("Restarting Level");
+        }
+
+        if (gravityOn)
+        {
+            sceneLoader -= Time.deltaTime;
+            if (sceneLoader <= 0f)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+                Debug.Log("Loading Next Level");
+                sceneLoader = 3f;
+                gravityOn = false;
+            }
+        }
+    }
+   
+
+    public void GravityTurnedOn()
+    {
+        gravityOn = true;
+        Physics.gravity = new Vector3(0, -9.81f, 0);
     }
 }
