261c9fa6635dd22a3e726522b32024d9f421913b
FerPresto <163638368+FerPresto@users.noreply.github.com>
2026-02-04 00:51:48 -0500

    AntigravityStarted

--- CosmicWageWorkers/Assets/Scripts/Anti Gravity Game/GravityManager.cs
+++ CosmicWageWorkers/Assets/Scripts/Anti Gravity Game/GravityManager.cs
@@ -4,10 +4,13 @@ public class GravityManager : MonoBehaviour
 {
 
     public AntiGravity antiGravityScript;
+    public float gravityScale = -1.0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        antiGravityScript.TriggerAntiGravity();
+        Physics.gravity = new Vector3(0, gravityScale, 0);
     }
 
     // Update is called once per frame
