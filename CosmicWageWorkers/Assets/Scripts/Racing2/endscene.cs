5cc529fcd244f0cdb338e3bdafaf07e9754fdd18
Waihong <tsangwaihong63@gmail.com>
2026-02-25 23:58:50 -0500

    fix camera switch bug on racing2
    fix camera switch bug on racing2

--- CosmicWageWorkers/Assets/Scripts/Racing2/endscene.cs
+++ CosmicWageWorkers/Assets/Scripts/Racing2/endscene.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 public class endscene : MonoBehaviour
 {
     public GameObject teen;
+    public GameObject origin_teen;
     public Camera c1;
     public Camera c2;
-    public Animator teenKick;
+    
 
     public void PlayAnim()
     {
         teen.SetActive(true);
+        origin_teen.SetActive(false);
 
         c1.gameObject.SetActive(false);
         c2.gameObject.SetActive(true);
-
-        teenKick.Play("teen_kicked", 0, 0f);
+        
     }
 }
