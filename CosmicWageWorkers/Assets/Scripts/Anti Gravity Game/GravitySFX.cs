3342cd7bee6838c749afb40ea96ee82b5c73bdc6
FerPresto <163638368+FerPresto@users.noreply.github.com>
2026-03-08 01:18:23 -0500

    FartImplementedAndFinishedTasklist

--- CosmicWageWorkers/Assets/Scripts/Anti Gravity Game/GravitySFX.cs
+++ CosmicWageWorkers/Assets/Scripts/Anti Gravity Game/GravitySFX.cs
@@ -8,6 +8,7 @@ public class GravitySFX : MonoBehaviour
     public AudioClip boost;
     public AudioClip obstacle;
     public AudioClip clickSFX;
+    public AudioClip fartSFX;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
