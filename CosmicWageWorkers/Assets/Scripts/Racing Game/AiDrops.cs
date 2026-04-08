8506e2a9df90cc31b1b261f2d3c1d2fc92df7a69
Jack Johnson <jjohnson9038@gmail.com>
2026-04-08 10:10:18 -0400

    poo
    ui poo

--- CosmicWageWorkers/Assets/Scripts/Racing Game/AiDrops.cs
+++ CosmicWageWorkers/Assets/Scripts/Racing Game/AiDrops.cs
@@ -1,20 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class AiDrops : MonoBehaviour
 {
     public float slowSpeed; // how slow the vehicle will slow down
+
+    [Header("UI Feedback")]
+    public Image hitImage;
+    public float imageDuration = 1f;
 
     public KartControllerArcade car;
     public KartControllerArcade currentCar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider other)
     {
-        KartControllerArcade kart = other.GetComponent<KartControllerArcade>();
+        KartControllerArcade kart = other.GetComponentInParent<KartControllerArcade>();
 
         if (kart != null)
         {
-            kart.ApplySlow(slowSpeed, 2f); // 2 seconds slowdown
-            Debug.Log("Slowed kart!");
+            kart.ApplySlow(slowSpeed, 2f);
+
+            if (hitImage == null)
+            {
+                GameObject obj = GameObject.Find("HitImage");
+                if (obj != null)
+                    hitImage = obj.GetComponent<Image>();
+            }
+
+            if (hitImage != null)
+            {
+                StartCoroutine(ShowHitImage());
+            }
+
+            //stroy(gameObject);
         }
+    }
+    private void Awake()
+    {
+        GameObject obj = GameObject.Find("poo");
+        hitImage = obj.GetComponent<Image>();
+    }
+    IEnumerator ShowHitImage()
+    {
+        
+        hitImage.enabled = true;
+        yield return new WaitForSeconds(imageDuration);
+        hitImage.enabled = false;
     }
 }
