6a6b0d4ea7c8b659a9ed793771b80746b0b35fcb
AlexThomUCF <al849783@ucf.edu>
2026-04-23 16:39:57 -0400

    Disable border AGFinal

--- CosmicWageWorkers/Assets/Scripts/SkipCutscene.cs
+++ CosmicWageWorkers/Assets/Scripts/SkipCutscene.cs
@@ -5,12 +5,19 @@ using UnityEngine.InputSystem;
 public class SkipCutscene : MonoBehaviour
 {
     public PlayableDirector director;
+    public GameObject border;
 
     void Update()
     {
         if (Keyboard.current.tKey.wasPressedThisFrame && director.state == PlayState.Playing)
         {
             director.Stop();
+            border.SetActive(false);
+        }
+
+        if(director.state != PlayState.Playing)
+        {
+            border.SetActive(false);
         }
     }
 }
