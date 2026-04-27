52bb9383e977d5938e21547a688607ff99dc99a8
FerPresto <163638368+FerPresto@users.noreply.github.com>
2026-04-27 10:05:59 -0400

    BathroomTextures

--- CosmicWageWorkers/Assets/Scripts/Bathroom Horror/PlayerReturnToStart.cs
+++ CosmicWageWorkers/Assets/Scripts/Bathroom Horror/PlayerReturnToStart.cs
@@ -4,8 +4,6 @@ public class PlayerReturnToStart : MonoBehaviour
 {
     public BathroomsUnhide bathroomUnhideScript;
     public Vector3 startingPosition;
-    public GameObject playerCamera;
-    public Quaternion startingRotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,7 +16,6 @@ public class PlayerReturnToStart : MonoBehaviour
         if (bathroomUnhideScript.playerReturning)
         {
             transform.position = startingPosition; // Change to your desired starting position
-            playerCamera.transform.rotation = startingRotation; // Change to your desired starting rotation
             bathroomUnhideScript.playerReturning = false; // Reset the flag after returning to start
         }
     }
